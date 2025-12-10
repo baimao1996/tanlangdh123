84b197799496e715e54baf1c3a4a1681c929c798
baimao1996 <3184683263@qq.com>
2025-12-11 01:13:03 +0800

    Add files via upload

---
+++ static/js/config4.js
@@ -0,0 +1,20 @@
+var BaseUrl =  "http://43.240.73.13:1992";
+var ResourceUrl = 'http://43.240.73.13:1996/tthaibei/';
+
+if(window.location.pathname!='/'
+    &&window.location.pathname!='/index.html'
+    &&window.location.pathname!='/site/login.html'
+    &&window.location.pathname!='/site/wxredd.html')
+{
+    var wwid=window.localStorage.getItem("wwid");
+    if(!wwid)
+    {
+        localStorage.setItem('enter_url',encodeURIComponent(window.location.href))
+
+        //alert('请先登录');
+        window.location.href = '/index.html';
+    }
+}
+
+
+        
